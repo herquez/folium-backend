e7ce75b67a615a225ff763f604625f43c3e2f485
herquez <miguelherquez@gmail.com>
2025-03-22 14:25:02 -0600

    Adds restTemplate and bad url test.

--- src/test/java/com/api/folium/FoliumApplicationTests.java
+++ src/test/java/com/api/folium/FoliumApplicationTests.java
@@ -1,13 +1,23 @@
 package com.api.folium;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FoliumApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
-	}
+	void shouldReturnNotFoundWithNonexistenceUrl() {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity("/badUrl", String.class);
 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 }

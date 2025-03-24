65f0902e9e27c31867054a85d62ebe688a7286d3
herquez <miguelherquez@gmail.com>
2025-03-23 20:28:38 -0600

    Tests H2 is used for testing.

--- src/test/java/com/api/folium/FoliumApplicationTests.java
+++ src/test/java/com/api/folium/FoliumApplicationTests.java
@@ -1,17 +1,31 @@
 package com.api.folium;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import javax.sql.DataSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FoliumApplicationTests {
 	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
 	TestRestTemplate restTemplate;
+
+	@Test
+	void testShouldUseH2Database() {
+		assertThat(dataSource).isInstanceOf(HikariDataSource.class);
+		String url = ((HikariDataSource) dataSource).getJdbcUrl();
+		assertThat(url).contains("h2:mem:testdb");
+	}
 
 	@Test
 	void shouldReturnNotFoundWithNonexistenceUrl() {
